4fbe84b252a0cdc054c991d8570fa6214f8d40f2
Harsh Pandey <harshap17058@gmail.com>
2026-04-28 12:15:14 +0530

    Added repositories

---
+++ java/com/studentlink/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.studentlink.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.studentlink.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
